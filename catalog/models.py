d941ffcdfbaccb4b80ceeeb00e94829baabf4fc4
Luis A. Nuñez <armandon20@gmail.com>
2018-06-04 16:18:07 -0500

    Add Book model to catalog app

--- catalog/models.py
+++ catalog/models.py
@@ -1,14 +1,51 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
 class Genre(models.Model):
     """
     Modelo que representa un género literario (p. ej. ciencia ficción, poesía, etc.).
     """
-    name = models.CharField(max_length=200, help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)")
-    
+    name = models.CharField(
+        max_length=200, help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)")
+
     def __str__(self):
         """
         Cadena que representa a la instancia particular del modelo (p. ej en el sitio de Administración)
         """
         return self.name
+
+
+class Book(models.Model):
+    """
+    Modelo que representa un libro (pero no un Ejemplar específico).
+    """
+
+    title = models.CharField(max_length=200)
+
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    # ForeignKey, ya que un libro tiene un solo autor, pero el mismo autor puede haber escrito muchos libros.
+    # 'Author' es un string, en vez de un objeto, porque la clase Author aún no ha sido declarada.
+
+    summary = models.TextField(
+        max_length=1000, help_text="Enter a brief description of the book")
+
+    isbn = models.CharField(
+        'ISBN', max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+
+    genre = models.ManyToManyField(
+        Genre, help_text="Select a genre for this book")
+    # ManyToManyField, porque un género puede contener muchos libros y un libro puede cubrir varios géneros.
+    # La clase Genre ya ha sido definida, entonces podemos especificar el objeto arriba.
+
+    def __str__(self):
+        """
+        String que representa al objeto Book
+        """
+        return self.title
+
+    def get_absolute_url(self):
+        """
+        Devuelve el URL a una instancia particular de Book
+        """
+        return reverse('book-detail', args=[str(self.id)])
