49deb5c23389cc583dec309b96551b9da55eaae0
Luis A. Nuñez <armandon20@gmail.com>
2018-06-04 16:10:35 -0500

    Add model Genre to catalog app

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Genre(models.Model):
+    """
+    Modelo que representa un género literario (p. ej. ciencia ficción, poesía, etc.).
+    """
+    name = models.CharField(max_length=200, help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)")
+    
+    def __str__(self):
+        """
+        Cadena que representa a la instancia particular del modelo (p. ej en el sitio de Administración)
+        """
+        return self.name
