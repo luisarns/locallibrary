ba006ab1973fb290ea42e4c3734fe4297160a129
Luis A. Nuñez <armandon20@gmail.com>
2018-08-13 22:08:58 -0500

    Add users and groups to catalog app

--- catalog/admin.py
+++ catalog/admin.py
@@ -4,14 +4,17 @@ from .models import Author, Genre, Book, BookInstance, Language
 admin.site.register(Genre)
 admin.site.register(Language)
 
+
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
 
+
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
+
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
