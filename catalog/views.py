81f98f1153e13ad4669c7fde199e3c29de448a88
Luis A. Nuñez <armandon20@gmail.com>
2018-09-08 16:38:57 -0500

    Add generic edit views for created, updated and deleted Authors

--- catalog/views.py
+++ catalog/views.py
@@ -2,12 +2,30 @@ import datetime
 
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from django.contrib.auth.decorators import permission_required
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from .models import Book, Author, BookInstance
 from catalog.forms import RenewBookForm
+
+
+class AuthorCreate(CreateView):
+    model = Author
+    fields = '__all__'
+    initial = {'date_of_birth': '05/01/2018'}
+
+
+class AuthorUpdate(UpdateView):
+    model = Author
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+
+
+class AuthorDelete(DeleteView):
+    model = Author
+    success_url = reverse_lazy('authors')
 
 
 # Create your views here.
